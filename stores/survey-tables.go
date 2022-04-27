cc57695988eac2aae7367f2dcea322ea0eef42c5
Triet Nguyen <triet.nguyen@usace.army.mil>
2022-04-27 14:26:12 +0000

    fixed bug - next_survey_order not tied to specific survey

--- stores/survey-tables.go
+++ stores/survey-tables.go
@@ -65,43 +65,80 @@ var surveyAssignmentTable = dq.TableDataSet{
 	Statements: map[string]string{
 		"updateAssignment": `update survey_assignment set completed='true' where id=$1`,
 		"assignSurvey":     `insert into survey_assignment (se_id,assigned_to) values ($1,$2) returning id`,
-		"assignmentInfo": `select sa_id,se_id,completed,survey_order,next_survey_order,next_survey_seid,next_control_order,next_control_seid from (
-
-								select t1.id as sa_id,t1.se_id,t1.completed,t2.survey_order, null as next_survey_order, null as next_survey_seid, null as next_control_order, null as next_control_seid
-								from survey_element t2
-								left outer join survey_assignment t1 on t1.se_id=t2.id
-								where assigned_to=$1 and t2.survey_id=$2 and completed='false'
-
-								union
-
-								select sa_id,se_id,completed,next_assignment.survey_order,next_survey_order, se1.id as next_survey_seid, next_control_order, se2.id as next_control_seid from
-									(select null::uuid as sa_id, null::uuid as se_id, null::bool as completed,null::integer as survey_order,
-
-									(select case when (
-											select max(t2.survey_order)
-											from survey_assignment t1
-											inner join survey_element t2 on t2.id=t1.se_id
-											where t2.survey_id=$2 and t2.is_control='false') is null
-										then
-											(select min(survey_order) from survey_element where survey_id=$2 and is_control='false')
-										else
-										(select min(survey_order) from survey_element where survey_order>
-											(select max(t2.survey_order)
-											from survey_assignment t1
-											inner join survey_element t2 on t2.id=t1.se_id
-											where t2.survey_id=$2 and t2.is_control='false')
-											and is_control='false')
-										end) as next_survey_order,
-
-									(select min(t1.survey_order) from survey_element t1
-											left outer join (select * from survey_assignment where assigned_to=$1) t2 on t1.id=t2.se_id
-											where assigned_to is null and is_control='true' and survey_id=$2) as next_control_order
+		"assignmentInfo": `
+			select 
+				sa_id,
+				se_id,
+				completed,
+				survey_order,
+				next_survey_order,
+				next_survey_seid,
+				next_control_order,
+				next_control_seid 
+			from (
+				select 
+					t1.id as sa_id,
+					t1.se_id,
+					t1.completed,
+					t2.survey_order, 
+					null as next_survey_order, 
+					null as next_survey_seid, 
+					null as next_control_order, 
+					null as next_control_seid 
+				from survey_element t2 
+				left outer join survey_assignment t1 on t1.se_id=t2.id 
+				where assigned_to=$1 and t2.survey_id=$2 and completed='false' 
+				union 
+				select 
+					sa_id,
+					se_id,
+					completed,
+					next_assignment.survey_order,
+					next_survey_order, 
+					se1.id as next_survey_seid, 
+					next_control_order, 
+					se2.id as next_control_seid 
+				from (
+					select 
+						null::uuid as sa_id, 
+						null::uuid as se_id, 
+						null::bool as completed,
+						null::integer as survey_order, 
+						(
+							select case when ( 
+								select max(t2.survey_order)
+								from survey_assignment t1
+								inner join survey_element t2 on t2.id=t1.se_id
+								where t2.survey_id=$2 and t2.is_control='false'
+							) is null then (
+								select min(survey_order) 
+								from survey_element 
+								where survey_id=$2 and is_control='false'
+							) else (
+								select min(survey_order) 
+								from survey_element where survey_order> (
+									select max(t2.survey_order)
+										from survey_assignment t1
+										inner join survey_element t2 on t2.id=t1.se_id
+										where t2.survey_id=$2 and t2.is_control='false'
+								) and is_control='false' and survey_id=$2
+							) end
+						) as next_survey_order, 
+						(
+							select min(t1.survey_order) 
+							from survey_element t1
+							left outer join (
+								select * 
+								from survey_assignment 
+								where assigned_to=$1
+							) t2 on t1.id=t2.se_id
+							where assigned_to is null and is_control='true' and survey_id=$2
+						) as next_control_order
 								) next_assignment
 								inner join survey_element se1 on se1.survey_order=next_assignment.next_survey_order
 								left outer join survey_element se2 on se2.survey_order=next_assignment.next_control_order
 								where se1.survey_id=$2 and (se2.survey_id=$2  or se2.survey_id is null)
 							) assignment_query
-
 							order by survey_order limit 1`,
 	},
 	Fields: models.SurveyAssignment{},
